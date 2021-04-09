5cf2fbb60258e0e25c1eac9493d3d00bc35c92a5
sangjun <sgbo5003@naver.com>
2021-04-09 13:36:00 +0900

    App 경로 재설정

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import App032601 from './App032601';
+import App from './app/App';
 import {name as appName} from './app.json';
+const TodoApp = () => <App />;
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TodoApp);
