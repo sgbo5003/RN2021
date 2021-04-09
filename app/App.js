bb0bb0fe09f3ce7d311246ac635ce1eb54679246
sangjun <sgbo5003@naver.com>
2021-04-09 15:18:21 +0900

    submitTodo 메서드 생성

--- app/App.js
+++ app/App.js
@@ -25,17 +25,55 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Heading from './Heading';
+import Input from './Input';
+
+let todoIndex = 0;
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All',
+    };
+  }
+
+  inputChange(inputValue) {
+    console.log(' Input Value: ', inputValue);
+    this.setState({inputValue});
+  }
+
+  submitTodo() {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return; //비어있으면 아무것도 반환 x
+    }
+    const todo = {
+      // 비어있지 않으면 todo 변수를 생성해서 다음의 객체를 할당
+      title: this.state.inputValue,
+      todoIndex,
+      complete: false,
+    };
+    todoIndex++; // index를 증가
+    const todos = [...this.state.todos, todo]; //새로운 todo를 기존 배열에 추가
+    this.setState({todos, inputValue: ''}, () => {
+      console.log('State: ', this.state);
+    });
   }
   render() {
+    const {inputValue} = this.state;
     return (
       <View style={styles.container}>
         <ScrollView
           keyboardShouldPersistTaps="always" // -> 키보드가 열려 있을 경우 이를 닫고 난 후에 onPress 이벤트를 처리하게 한다.
-          style={styles.content}></ScrollView>
+          style={styles.content}>
+          <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={text => this.inputChange(text)}
+          />
+        </ScrollView>
       </View>
     );
   }
