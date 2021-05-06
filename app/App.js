f15059f9020f59d7cd2f3a5377b461365ae512f0
sangjun <sgbo5003@naver.com>
2021-05-06 18:38:49 +0900

    export styles

--- app/App.js
+++ app/App.js
@@ -1,127 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Text, View, TouchableHighlight} from 'react-native';
+import {styles, buttons} from './styles';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Heading from './Heading';
-import Input from './Input';
-import Button from './Button';
-import TodoList from './TodoList';
-import TabBar from './TapBar';
-
-let todoIndex = 0;
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      inputValue: '',
-      todos: [],
-      type: 'All',
-    };
-    this.submitTodo = this.submitTodo.bind(this);
-    this.toggleComplete = this.toggleComplete.bind(this);
-    this.deleteTodo = this.deleteTodo.bind(this);
-    this.setType = this.setType.bind(this);
-  }
-
-  setType(type) {
-    this.setState({type});
-  }
-
-  inputChange(inputValue) {
-    console.log(' Input Value: ', inputValue);
-    this.setState({inputValue});
-  }
-
-  submitTodo() {
-    if (this.state.inputValue.match(/^\s*$/)) {
-      return; //비어있으면 아무것도 반환 x
-    }
-    const todo = {
-      // 비어있지 않으면 todo 변수를 생성해서 다음의 객체를 할당
-      title: this.state.inputValue,
-      todoIndex,
-      complete: false,
-    };
-    todoIndex++; // index를 증가
-    const todos = [...this.state.todos, todo]; //새로운 todo를 기존 배열에 추가
-    this.setState({todos, inputValue: ''}, () => {
-      console.log('State: ', this.state);
-    });
-  }
-
-  deleteTodo(todoIndex) {
-    let {todos} = this.state;
-    todos = todos.filter(todo => todo.todoIndex !== todoIndex);
-    this.setState({todos});
-  }
-  toggleComplete(todoIndex) {
-    let todos = this.state.todos;
-    todos.forEach(todo => {
-      if (todo.todoIndex === todoIndex) {
-        todo.complete = !todo.complete;
-      }
-    });
-    this.setState({todos});
-  }
+export default class App extends Component {
   render() {
-    const {inputValue, todos, type} = this.state;
     return (
       <View style={styles.container}>
-        <ScrollView
-          keyboardShouldPersistTaps="always" // -> 키보드가 열려 있을 경우 이를 닫고 난 후에 onPress 이벤트를 처리하게 한다.
-          style={styles.content}>
-          <Heading />
-          <Input
-            inputValue={inputValue}
-            inputChange={text => this.inputChange(text)}
-          />
-          <TodoList
-            type={type}
-            toggleComplete={this.toggleComplete}
-            deleteTodo={this.deleteTodo}
-            todos={todos}
-          />
-          <Button submitTodo={this.submitTodo} />
-        </ScrollView>
-        <TabBar type={type} setType={this.setType} />
+        <TouchableHighlight style={buttons.primary}>
+          <Text>Sample Text!!</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-  content: {
-    flex: 1,
-    paddingTop: 60,
-  },
-});
-
-export default App;
