2c39b966b901544648dc3f4e76276e095e32de53
sangjun <sgbo5003@naver.com>
2021-06-10 20:12:07 +0900

    redux

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './app/App';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
